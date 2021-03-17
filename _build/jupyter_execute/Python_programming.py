8f9f0498bee5d37efa64d94e8e448cfec8cdd91a
Andrew Stewart <drandrewjstewart@gmail.com>
2021-03-17 12:12:45 +0000

    add video link

---
+++ _build/jupyter_execute/Python_programming.py
@@ -0,0 +1,10 @@
+# Introduction to Python Programming
+
+from IPython.display import HTML
+
+HTML('<iframe width="560" height="315" src="https://youtube.com/embed/HjF98JryayQ" frameborder="0" allowfullscreen></iframe>')
+
+%%HTML
+<div style="text-align: center">
+<iframe width="560" height="315" src="https://youtube.com/embed/HjF98JryayQ" frameborder="0" allowfullscreen></iframe>
+</div>
