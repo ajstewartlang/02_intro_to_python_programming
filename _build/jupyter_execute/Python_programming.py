1f007813b4027f09028a4ca06df61f90382a2fd3
Andrew Stewart <drandrewjstewart@gmail.com>
2021-04-29 10:39:54 +0100

    auto commit

--- _build/jupyter_execute/Python_programming.py
+++ _build/jupyter_execute/Python_programming.py
@@ -8,6 +8,12 @@ In this second workshop we'll take our first look at programming in Python. We'l
 
 </center>
 
+There are a couple of really good introductory books on using Python for data science that I recommend.
+
+[![wes_mckinney_book](images/python_data_analysis.jpg)](https://www.amazon.co.uk/Python-Data-Analysis-Wes-Mckinney-dp-1491957662/dp/1491957662/ref=dp_ob_title_bk) 
+
+[![Jake VanderPlas_book](images/data_science_handbook.jpg)](https://www.amazon.co.uk/Python-Data-Science-Handbook-Techniques/dp/1491912057/ref=sr_1_1?crid=KFHZFNZYHQTL&dchild=1&keywords=python+data+science+handbook&qid=1619688682&s=books&sprefix=python+data+science+%2Cstripbooks%2C157&sr=1-1)
+
 %%HTML
 <div style="text-align: center">
 <iframe width="560" height="315" src="https://youtube.com/embed/HjF98JryayQ" frameborder="0" allowfullscreen></iframe>
